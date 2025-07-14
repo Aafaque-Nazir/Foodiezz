54d88419576dd5fc6166d4d3bde52e4cd406df4b
Aafaque Nazir <aafaquenazir@gmail.com>
2025-07-15 01:58:15 +0530

    price update

--- data/MenuItems.jsx
+++ data/MenuItems.jsx
@@ -1,21 +1,21 @@
 const menuItems = [
   {
     name: "Mayo Chicken Veggie Cheese Grilled Sandwich",
-    price: 129,
+    price: 139,
     category: "Non-Veg",
     image: "mayovc.jpg",
     link: "https://zomato.onelink.me/xqzv/n2iylwls"
   },
   {
     name: " Chicken Cheese Grilled Sandwich ",
-    price: 139,
+    price: 149,
     category: "Non-Veg",
     image: "chickencheese.jpg",
     link: "https://zomato.onelink.me/xqzv/fujxiyuf"
   },
   {
     name: "Tandoori Chicken Cheese Grilled Sandwich",
-    price: 149,
+    price: 159,
     category: "Non-Veg",
     image: "tandoori.jpg",
     link: "https://zomato.onelink.me/xqzv/9afyi2m5"
@@ -23,14 +23,14 @@ const menuItems = [
  
   {
     name: "Classic Veg Cheese Grilled Sandwich",
-    price: 109,
+    price: 99,
     category: "Veg",
     image: "classicveg.jpg",
     link: "https://zomato.onelink.me/xqzv/ampfos2q"
   },
   {
     name: "Mayo Veggie Cheese Grilled Sandwich",
-    price: 99,
+    price: 109,
     category: "Veg",
     image: "mayoveggie.jpg",
     link: "https://zomato.onelink.me/xqzv/yn98a3am"
@@ -44,28 +44,28 @@ const menuItems = [
   },
   {
     name: "Classic Chicken Mayo Sandwich",
-    price: 129,
+    price: 119,
     category: "Non-Veg",
     image: "mayo.jpg",
     link: "https://zomato.onelink.me/xqzv/mmxtp1w2"
   },
   {
     name: "Tikka Tandoori Biryani",
-    price: 269,
+    price: 219,
     category: "Biryani (Non-Veg)",
     image: "biryani.jpg",
     link: "https://zomato.onelink.me/xqzv/w9rijk9m"
   },
   {
     name: "Tandoori Pizza [ 7 inches ]",
-    price: 249,
+    price: 169,
     category: "Pizza (Non-Veg)",
     image: "Tpizza.jpg",
     link: "https://zomato.onelink.me/xqzv/gdsn30tc"
   },
   {
     name: "Cheese loaded Tandoori Pizza [ 7 inches ]",
-    price: 279,
+    price: 179,
     category: "Pizza (Non-Veg)",
     image: "Cpizza.jpg",
     link: "https://zomato.onelink.me/xqzv/jw9ig7ax"
